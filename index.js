ad98615c996265bed8444eec8e68b73860e824dc
jkeslar21 <diamondroad91@gmail.com>
2019-03-21 10:18:19 -0700

    makesPortDynamic

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// top spot recommended for 'dontenv' import (put at top of page anywhere environment (env) values are used)
+require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// make the port dynamic #1
+// use process.env.PORT otherwise use 4000
+// yarn add dotenv
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING
+server.listen(port, () => {
+  console.log(`\n*** ${greeting} on http://localhost:${port} ***\n`);
 });
