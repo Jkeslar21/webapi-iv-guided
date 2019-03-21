a82d60b54bf2538c934cc50bb852958f18e3d125
jkeslar21 <diamondroad91@gmail.com>
2019-03-21 11:14:00 -0700

    changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = require('./api/server.js');
 // make the port dynamic #1
 // use process.env.PORT otherwise use 4000
 // yarn add dotenv  
+// .env will only work locally (gitignore will remove .env when deployed)
+// heroku.=> settings => config vars => GREETING: hellow from heroku (key:value)
 const port = process.env.PORT || 4000;
 const greeting = process.env.GREETING
 server.listen(port, () => {
